91a8859a4e74e65950a40d726866ca69eaadbd83
Ghantoos <ghnatios1@gmail.com>
2023-04-04 11:57:45 +0300

    BackEnd <> Added initial table

---
+++ BackEnd/database/migrations/2023_04_04_082530_create_users_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->integer("organization_id")->unique();
+            $table->integer("gender");
+            $table->integer("user_type_id");
+            $table->integer('user_age');
+            $table->string('user_position');
+            $table->boolean('is_registered');
+            $table->boolean('is_active');
+            $table->integer('branch_id');
+            $table->string('user_profile_pic')->nullable();
+            $table->date('user_end_date')->nullable();
+            $table->date('user_start_date');
+            $table->string('password');
+            $table->string('username');
+            $table->string('user_bio')->nullable();
+            $table->string('field1')->nullable();
+            $table->string('field2')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
