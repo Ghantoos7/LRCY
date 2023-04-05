806e21e41207eb482d9f89d7776a2b58e96cfc0f
Theresa <theresaroukaibe1@outlook.com>
2023-04-05 13:59:36 +0300

    FrontEnd <> Worked on gallery navbar

--- FrontEnd/src/app/pages/gallery.page.ts
+++ FrontEnd/src/app/pages/gallery.page.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'app-gallery',
+  templateUrl: 'gallery.page.html',
+  styleUrls: ['gallery.page.scss'],
   standalone: true,
   imports: [IonicModule],
 })
