69874d69db34986781e19dbeec94c7b6cd905ae3
Ghantoos <ghnatios1@gmail.com>
2023-04-06 20:57:20 +0300

    BackEnd <> Added user routes

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,31 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(["prefix" => "v0.1"], function(){
+
+    Route::group(["prefix" => "user"], function(){
+
+        Route::post("signup", [UserController::class, "signup"]);
+        Route::post("login", [UserController::class, "login"]);
+        Route::post("register", [UserController::class, "register"]);
+        Route::post("recover_request", [UserController::class, "recover_request"]);
+        Route::post("change_password", [UserController::class, "change_password"]);
+        Route::post("edit_profile", [UserController::class, "edit_profile"]);
+        Route::get("get_user_info/{user_id?}", [UserController::class, "get_user_info"]);
+        Route::get("get_total_trainings/{user_id}", [UserController::class, "get_trainings_count"]);
+        Route::get("get_total_volunteering_time/{user_id}", [UserController::class, "get_total_volunteering_time"]);
+        Route::get("get_total_posts/{user_id?}", [UserController::class, "get_total_posts"]);
+        Route::get("get_total_comments/{user_id}", [UserController::class, "get_total_comments"]);
+        Route::get("get_total_likes_received/{user_id}", [UserController::class, "get_total_likes_received"]);
+        Route::get("get_events_organized/{user_id}", [UserController::class, "get_events_organized"]);
+        Route::get("get_total_trainings_y&h/{user_id}", [UserController::class, "get_total_trainings_y&h"]);
+        Route::get("get_total_trainings_hvp/{user_id}", [UserController::class, "get_total_trainings_hvp"]);
+        Route::get("get_total_trainings_env/{user_id}", [UserController::class, "get_total_trainings_env"]);
+        Route::get("get_total_trainings_left/{user_id}", [UserController::class, "get_total_trainings_left"]);
+        Route::get("get_own_posts/{user_id}", [UserController::class, "get_own_posts"]);
+
+    });
 });
