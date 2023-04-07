dee04311315fe56cc7a357f384cdcab61910a29f
Joe Wehbe <102875229+Joe-Wehbe@users.noreply.github.com>
2023-04-07 19:09:31 +0300

    FrontEnd <> Started designing the sign-up page

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -29,6 +29,14 @@ export const routes: Routes = [
     path: 'my-posts',
     loadComponent: () => import('./pages/my-posts/my-posts.page').then( m => m.MyPostsPage)
   },
+  {
+    path: 'sign-up',
+    loadComponent: () => import('./pages/sign-up/sign-up.page').then( m => m.SignUpPage)
+  },
+  {
+    path: 'sign-in',
+    loadComponent: () => import('./pages/sign-in/sign-in.page').then( m => m.SignInPage)
+  },
  
 ];
 
