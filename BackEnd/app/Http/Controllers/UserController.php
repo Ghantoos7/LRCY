8010e69ffdc9a7cbb5f61e6df3551fb2bf4a9fc0
NajatN <121221757+NajatN@users.noreply.github.com>
2023-04-07 08:33:09 +0300

    BackEnd <> Clean and optimize signup API

--- BackEnd/app/Http/Controllers/UserController.php
+++ BackEnd/app/Http/Controllers/UserController.php
@@ -11,25 +11,23 @@ class UserController extends Controller
 
 
     function signup(Request $request) {
+        
+        // Get the organization ID from the request
         $organization_id = $request->input('organization_id');
+
+        // Retrieve a volunteer user record from the database based on the `organization_id` input parameter
         $existing_volunteer_user = volunteer_user::where('organization_id', '=', $organization_id)->first();
-        if($existing_volunteer_user) {
-            if($existing_volunteer_user->is_registered == 0) {
-                return response()->json([
-                    'status' => 'Organization ID found, user not registered'
-                ]);
-            }
-            else {
-                return response()->json([
-                    'status' => 'Organization ID found, user already registered'
-                ]);
-            }
-        }
-        else {
-            return response()->json([
-                'status' => 'Organization ID not found'
-            ]);
-        }
+        
+        // Determine the status message based on the existence and registration status of the volunteer user
+        $status = $existing_volunteer_user ? ($existing_volunteer_user->is_registered ? 'Organization ID found, user already registered' : 'Organization ID found, user not registered') : 'Organization ID not found';
+        
+        // Return a JSON response with the status message and appropriate HTTP status code
+        return response()->json([
+
+            'status' => $status
+
+        ], $existing_volunteer_user ? 200 : 404);
+
     }
 
    
