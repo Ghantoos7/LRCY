fe98b4e6a7090b34799d9fc0bfee2a39e9d0ead4
Theresa <theresaroukaibe1@outlook.com>
2023-04-10 18:48:35 +0300

    FrontEnd <> Started splash screen

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('../app/pages/splash-screen/splash-screen.page').then(m => m.SplashScreenPage)
+  },
+  {
     path: 'gallery',
     loadChildren: () => import('../app/pages/gallery/gallery.routes').then((m) => m.routes),
   },
@@ -31,6 +35,11 @@ export const routes: Routes = [
     path: 'sign-up',
     loadComponent: () => import('./pages/sign-up/sign-up.page').then( m => m.SignUpPage)
   },
+  {
+    path: 'splashScreen',
+    loadComponent: () => import('./pages/splash-screen/splash-screen.page').then( m => m.SplashScreenPage)
+  },
+
  
 ];
 
