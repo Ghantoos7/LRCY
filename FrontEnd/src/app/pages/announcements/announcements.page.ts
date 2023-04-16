c01630c4823d6caadfc962713fee914aa8cb2cb7
Theresa <theresaroukaibe1@outlook.com>
2023-04-16 19:12:17 +0300

    FrontEnd <> Finalized announcements

--- FrontEnd/src/app/pages/announcements/announcements.page.ts
+++ FrontEnd/src/app/pages/announcements/announcements.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-announcements',
   templateUrl: './announcements.page.html',
@@ -12,13 +12,41 @@ import { IonicModule } from '@ionic/angular';
 })
 export class AnnouncementsPage implements OnInit {
   showDescriptions: boolean[] = [];
-  constructor() { 
+
+  constructor(private router:Router) { 
     this.showDescriptions = new Array(3).fill(false);
   }
 
   ngOnInit() {
 
   }
+
+  goProfile(){
+    this.router.navigate(['/profile']);
+      }
+    
+      goFeed(){
+        this.router.navigate(['/feed']);
+      }
+      
+      goGoals(){
+        this.router.navigate(['/yearly-goals']);
+      }
+    
+      goGallery(){
+        this.router.navigate(['/gallery']);
+      }
+    
+      goAnnouncements(){
+        this.router.navigate(['/announcements']);
+      }
+    
+      toggleDarkMode(){
+    
+      }
+      logout(){
+      
+      }
 
   toggleDescription(index: number) {
     this.showDescriptions[index] = !this.showDescriptions[index];
