964d59be1e23632dde167471fabe981bc2de9374
Theresa <theresaroukaibe1@outlook.com>
2023-04-16 19:01:06 +0300

    FrontEnd <> Finished routing navbar

--- FrontEnd/src/app/pages/yearly-goals/yearly-goals.page.ts
+++ FrontEnd/src/app/pages/yearly-goals/yearly-goals.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-yearly-goals',
@@ -12,7 +13,7 @@ import { IonicModule } from '@ionic/angular';
 })
 export class YearlyGoalsPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
