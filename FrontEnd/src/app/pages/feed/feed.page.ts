95fef84e2da6dd78a43bfab2f349ba7a064898e9
Theresa <theresaroukaibe1@outlook.com>
2023-04-16 19:36:44 +0300

    FrontEnd <> Finalized navbar routing

--- FrontEnd/src/app/pages/feed/feed.page.ts
+++ FrontEnd/src/app/pages/feed/feed.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.page.html',
@@ -12,9 +12,35 @@ import { IonicModule } from '@ionic/angular';
 })
 export class FeedPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
 
+  goProfile(){
+    this.router.navigate(['/profile']);
+      }
+    
+      goFeed(){
+        this.router.navigate(['/feed']);
+      }
+      
+      goGoals(){
+        this.router.navigate(['/yearly-goals']);
+      }
+    
+      goGallery(){
+        this.router.navigate(['/gallery']);
+      }
+    
+      goAnnouncements(){
+        this.router.navigate(['/announcements']);
+      }
+    
+      toggleDarkMode(){
+    
+      }
+      logout(){
+      
+      }
 }
