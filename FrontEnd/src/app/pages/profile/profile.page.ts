729c305d0cec221397c3fa0ccd4efb8467813433
Theresa <theresaroukaibe1@outlook.com>
2023-04-16 17:56:57 +0300

    FrontEnd <> Linked button to form

--- FrontEnd/src/app/pages/profile/profile.page.ts
+++ FrontEnd/src/app/pages/profile/profile.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { Route, Router } from '@angular/router';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
@@ -12,9 +12,13 @@ import { IonicModule } from '@ionic/angular';
 })
 export class ProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  goToEditForm(){
+this.router.navigate(['edit-profile']);
   }
 
 }
