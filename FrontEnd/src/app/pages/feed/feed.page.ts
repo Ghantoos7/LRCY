0205c87f48c93090a126098228b28b88a1902594
Joe Wehbe <102875229+Joe-Wehbe@users.noreply.github.com>
2023-04-17 10:52:47 +0300

    FrontEnd <> Worked on the profile alert dialog

--- FrontEnd/src/app/pages/feed/feed.page.ts
+++ FrontEnd/src/app/pages/feed/feed.page.ts
@@ -1,46 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.page.html',
   styleUrls: ['./feed.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
+  encapsulation: ViewEncapsulation.None
 })
+
 export class FeedPage implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private alertController: AlertController) { }
+
+  async showProfile() {
+    const alert = await this.alertController.create({
+      header: 'Profile',
+      message: '<img src="../../assets/imgs/profile-picture.png" alt="Image description">',
+      cssClass: 'my-custom-class',
+      buttons: ['View Profile']
+    });
+  
+    await alert.present();
+  }
 
   ngOnInit() {
   }
 
   goProfile(){
     this.router.navigate(['/profile']);
-      }
+  }
     
-      goFeed(){
-        this.router.navigate(['/feed']);
-      }
-      
-      goGoals(){
-        this.router.navigate(['/yearly-goals']);
-      }
-    
-      goGallery(){
-        this.router.navigate(['/gallery']);
-      }
-    
-      goAnnouncements(){
-        this.router.navigate(['/announcements']);
-      }
-    
-      toggleDarkMode(){
-    
-      }
-      logout(){
-      
-      }
+  goFeed(){
+    this.router.navigate(['/feed']);
+  }
+  
+  goGoals(){
+    this.router.navigate(['/yearly-goals']);
+  }
+
+  goGallery(){
+    this.router.navigate(['/gallery']);
+  }
+
+  goAnnouncements(){
+    this.router.navigate(['/announcements']);
+  }
+
+  toggleDarkMode(){
+
+  }
+  logout(){
+  
+  }
 }
