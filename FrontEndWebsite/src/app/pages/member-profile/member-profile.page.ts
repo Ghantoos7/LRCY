ccb42a7fc1f73a2f6556b0bfce99363704fbccec
Theresa <theresaroukaibe1@outlook.com>
2023-04-18 21:19:29 +0300

    FrontEndWebsite <> Added fields

--- FrontEndWebsite/src/app/pages/member-profile/member-profile.page.ts
+++ FrontEndWebsite/src/app/pages/member-profile/member-profile.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-member-profile',
   templateUrl: './member-profile.page.html',
@@ -12,9 +12,13 @@ import { IonicModule } from '@ionic/angular';
 })
 export class MemberProfilePage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  exitPage(){
+    this.router.navigate(['/manage-profiles']);
   }
 
 }
