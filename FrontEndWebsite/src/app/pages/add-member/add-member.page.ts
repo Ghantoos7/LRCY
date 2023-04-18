ab9d0f4780a7be366e905bc4052174c3c8722e7a
Theresa <theresaroukaibe1@outlook.com>
2023-04-18 21:56:16 +0300

    FrontEndWebsite <> Finished add member form

--- FrontEndWebsite/src/app/pages/add-member/add-member.page.ts
+++ FrontEndWebsite/src/app/pages/add-member/add-member.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-add-member',
   templateUrl: './add-member.page.html',
@@ -12,9 +12,13 @@ import { IonicModule } from '@ionic/angular';
 })
 export class AddMemberPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  goBack(){
+    this.router.navigate(['/manage-profiles']);
   }
 
 }
