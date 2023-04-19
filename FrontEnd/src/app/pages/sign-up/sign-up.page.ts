f4c26c809abaefe37e1a5022e819a65fdef44f7f
Theresa <theresaroukaibe1@outlook.com>
2023-04-19 13:17:21 +0300

    FrontEnd <> Fixed bugs in pages layout

--- FrontEnd/src/app/pages/sign-up/sign-up.page.ts
+++ FrontEnd/src/app/pages/sign-up/sign-up.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.page.html',
@@ -12,9 +12,13 @@ import { IonicModule } from '@ionic/angular';
 })
 export class SignUpPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  goBack(){
+    this.router.navigate(['/sign-in'])
   }
 
 }
