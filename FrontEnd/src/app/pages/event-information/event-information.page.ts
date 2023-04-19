c839e7223db4bfd1af3a5dd83c8d6bbaccf243d3
Theresa <theresaroukaibe1@outlook.com>
2023-04-19 23:03:56 +0300

    ApplicationLinking <> Started linking event details

--- FrontEnd/src/app/pages/event-information/event-information.page.ts
+++ FrontEnd/src/app/pages/event-information/event-information.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { EventService } from 'src/app/services/event.service';
 @Component({
   selector: 'app-event-information',
   templateUrl: './event-information.page.html',
@@ -11,10 +12,21 @@ import { Router } from '@angular/router';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class EventInformationPage implements OnInit {
-
-  constructor(private router: Router) { }
+  my_id: string = "";
+  events: any = [];
+  constructor(private service:EventService, private router: Router) { }
 
   ngOnInit() {
+    const data = this.router.getCurrentNavigation()?.extras.state;
+    const event_id = JSON.stringify(data);
+    const id = JSON.parse(event_id)["id"];
+    this.my_id = id;
+    this.service.get_event(this.my_id).subscribe(response => {
+      const info = JSON.stringify(response);
+      this.events = response;
+ 
+   
+    });
   }
 
   goGallery(){
