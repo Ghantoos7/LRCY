85b8fbb4a7c4ed2810afa856dfab8f4c61d7ad39
NajatN <121221757+NajatN@users.noreply.github.com>
2023-04-21 09:47:11 +0300

    ApplicationLinking <> Modified edit profile page and linked edit profile API

--- FrontEnd/src/app/pages/edit-profile/edit-profile.page.ts
+++ FrontEnd/src/app/pages/edit-profile/edit-profile.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { AlertController, IonicModule } from '@ionic/angular';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -13,9 +15,55 @@ import { IonicModule } from '@ionic/angular';
 })
 export class EditProfilePage implements OnInit {
 
-  constructor() { }
+  user:any = [];
+  first_name:string = '';
+  last_name:string = '';
+  full_name:string = '';
+  username:string = '';
+  user_bio:string = '';
+  user_position:string = '';
+
+  constructor(private service:UserService, private alertController:AlertController, private router:Router) { }
 
   ngOnInit() {
+    this.service.get_user('1', '1').subscribe(response => {
+      this.user = response;
+      this.last_name = (this.user['user'].last_name);
+      this.first_name = (this.user['user'].first_name);
+      this.full_name = this.first_name + ' ' + this.last_name;
+      this.username = (this.user['user'].username);
+      this.user_bio = (this.user['user'].user_bio);
+      this.user_position = (this.user['user'].user_position);
+    });
   }
+
+  editProfileWithoutPic(username: string, user_bio: string){
+    this.service.editProfile(username, user_bio).subscribe(response => {
+      const parsedResponse = JSON.parse(JSON.stringify(response));
+      if (parsedResponse.status === 'success') {
+        const alert = this.alertController.create({
+          header: 'Success',
+          message: 'Profile updated successfully.',
+          buttons: ['OK']
+        });
+
+        alert.then(res => {
+          res.present();
+          this.router.navigate(['/profile']); // navigate to profile page after successful update
+        });
+      } else {
+        const alert = this.alertController.create({
+          header: 'Error',
+          message: 'An error occurred while updating your profile. Please try again later.',
+          buttons: ['OK']
+        });
+
+        alert.then(res => {
+          res.present();
+        });
+      }
+    });
+}
+
 
 }
