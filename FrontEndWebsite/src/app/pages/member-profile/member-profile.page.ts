4a38669dc76c22733f4f0dca31c69204ce3cee86
Joe Wehbe <102875229+Joe-Wehbe@users.noreply.github.com>
2023-04-23 16:07:28 +0300

    FrontEndWebsite <> Finished adjusting member-profile

--- FrontEndWebsite/src/app/pages/member-profile/member-profile.page.ts
+++ FrontEndWebsite/src/app/pages/member-profile/member-profile.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
+
 @Component({
   selector: 'app-member-profile',
   templateUrl: './member-profile.page.html',
@@ -12,13 +14,37 @@ import { Router } from '@angular/router';
 })
 export class MemberProfilePage implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private menuCtrl: MenuController) { }
 
   ngOnInit() {
   }
 
   exitPage(){
     this.router.navigate(['/manage-profiles']);
+  }
+
+  goToAddTraining(){
+    this.router.navigate(['/add-training'])
+  }
+  
+  goToDeleteTraining(){
+    this.router.navigate(['/delete-training']);
+  }
+
+  closeMenu() {
+    this.menuCtrl.close();
+  }
+
+  goToManageProfiles(){
+    this.router.navigate(['/manage-profiles']);
+  }
+
+  goToHome(){
+    this.router.navigate(['/home']);
+  }
+
+  goToPanel(){
+    this.router.navigate(['/panel']);
   }
 
 }
