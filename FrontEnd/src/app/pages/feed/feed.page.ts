7a07487bdff6bee948e676dc2e40af2497362aa0
Theresa <theresaroukaibe1@outlook.com>
2023-04-25 21:25:43 +0300

    ApplicationLinking <> Fixed bugs in liking

--- FrontEnd/src/app/pages/feed/feed.page.ts
+++ FrontEnd/src/app/pages/feed/feed.page.ts
@@ -25,10 +25,11 @@ export class FeedPage implements OnInit {
   user_id = localStorage.getItem('userId') as string;
   posts: any;
   index: number=0;
-  isLiked: {[key: number]: boolean} = {};
+  isLikedUser: { [key: number]: boolean } = {};
   post_id: number=0;
   comment_contents: any=[];
   user: any;
+  isLiked: { [key: number]: { postId: number, isLiked: boolean }[] } = {};
 
   constructor(private router:Router, private alertController: AlertController, private menuCtrl: MenuController, private service:PostService, private sharedService:SharedService, private userservice:UserService) { }
 
@@ -59,13 +60,15 @@ export class FeedPage implements OnInit {
   }  
 
   ngOnInit() {
+
     this.service.getPosts().subscribe((data: any) => {
       this.posts = data['posts'];
       for (let i = 0; i < this.posts.length; i++) {
         const postId = this.posts[i].id;
-        this.isLiked[postId] = localStorage.getItem(`post_${postId}`) === 'true'; // retrieve the like state from Local Storage
-      }
+        this.isLikedUser[postId] = localStorage.getItem(`user_${this.user_id}_post_${postId}`) === 'true'; // retrieve the like state from Local Storage
+      };
     });
+    console.log(this.isLikedUser);
   }
 
   getDaysAgo(postDate: string) {
@@ -143,7 +146,7 @@ export class FeedPage implements OnInit {
    
   }
   toggleLike(post_id: number) {
-    if (this.isLiked[post_id]) {
+    if (this.isLikedUser[post_id]) {
     this.unlikePost(post_id);
     } else {
     this.likePost(post_id);
@@ -152,8 +155,8 @@ export class FeedPage implements OnInit {
     
   likePost(post_id: number) {
     this.service.likePost(post_id).subscribe((data: any) => {
-      localStorage.setItem(`post_${post_id}`, 'true'); // store the like state in Local Storage
-      this.isLiked[post_id] = true;
+      localStorage.setItem(`user_${this.user_id}_post_${post_id}`, 'true'); // store the like state in Local Storage
+      this.isLikedUser[post_id] = true;
       window.location.reload();
     });
    
@@ -161,8 +164,8 @@ export class FeedPage implements OnInit {
   
   unlikePost(post_id: number) {
     this.service.unlikePost(post_id).subscribe((data: any) => {
-      localStorage.setItem(`post_${post_id}`, 'false'); // store the like state in Local Storage
-      this.isLiked[post_id] = false;
+      localStorage.setItem(`user_${this.user_id}_post_${post_id}`, 'false'); // store the like state in Local Storage
+      this.isLikedUser[post_id] = false;
       window.location.reload();
     });
    
