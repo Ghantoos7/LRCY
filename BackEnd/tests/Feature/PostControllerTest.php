53d0a663362fbd7171727c1d4a87d0d85d5b6847
Georgio Ghnatios <94653514+Ghantoos7@users.noreply.github.com>
2023-05-01 02:34:54 +0300

    Testing <> Tested create post API

--- BackEnd/tests/Feature/PostControllerTest.php
+++ BackEnd/tests/Feature/PostControllerTest.php
@@ -4,17 +4,51 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
 use Tests\TestCase;
+use App\Models\Post;
+use App\Models\Post_type;
+use App\Models\Volunteer_user;
+use App\Models\Like;
+use App\Models\Comment;
+use App\Models\Reply;
+use App\Models\Comment_like;
 
 class PostControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase, WithoutMiddleware;
+    use WithFaker;
 
-        $response->assertStatus(200);
+    public function testCreatePostApi()
+    {
+        // Create a user for testing
+        $user = Volunteer_user::factory()->create();
+
+        // Test creating a text post
+        $textPostData = [
+            'user_id' => $user->id,
+            'post_type' => 'text',
+            'post_caption' => 'This is a text post',
+        ];
+        $responseText = $this->postJson('/api/v0.1/post/create_post', $textPostData);
+        $responseText->assertStatus(200);
+
+        // Test creating an image post
+        $imagePath = __DIR__ . '/test-image.jpg';
+        $image = new UploadedFile($imagePath, 'test-image.jpg', 'image/jpeg', null, true);
+        $imagePostData = [
+            'user_id' => $user->id,
+            'post_type' => 'image',
+            'post_caption' => 'This is an image post',
+            'post_media' => $image,
+        ];
+        $responseImage = $this->postJson('/api/v0.1/post/create_post', $imagePostData);
+        $responseImage->assertStatus(200);
     }
+    
 }
