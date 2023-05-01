c6171e08ff5dd7b19938d893bdd0a45c57c8f1cf
NajatN <121221757+NajatN@users.noreply.github.com>
2023-05-01 09:52:39 +0300

    ApplicationLinking <> Added media preview in edit post page

--- FrontEnd/src/app/pages/edit-post/edit-post.page.ts
+++ FrontEnd/src/app/pages/edit-post/edit-post.page.ts
@@ -6,6 +6,7 @@ import { Router } from '@angular/router';
 import { PostService } from 'src/app/services/post.service';
 import { AlertController } from '@ionic/angular';
 import { NavController } from '@ionic/angular';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-edit-post',
@@ -22,7 +23,11 @@ export class EditPostPage implements OnInit {
   
   post_src_img: any;
 
-  constructor(private alrt:AlertController, private postService:PostService, private router:Router) { }
+  post_src_video: any;
+
+  media_type: number = 0;
+
+  constructor(private alrt:AlertController, private postService:PostService, private router:Router, private sanitizer:DomSanitizer) { }
 
   ngOnInit() {
     const data = this.router.getCurrentNavigation()?.extras.state;
@@ -30,17 +35,35 @@ export class EditPostPage implements OnInit {
     const id = JSON.parse(post_id)["p_id"];
     this.post_id = id;
     this.postService.getPost(id).subscribe((data: any) => {
+      console.log(post_id);
+      console.log(data['post']);
       this.post_caption = data['post'].post_caption;
-      this.post_src_img = data['post'].post_media;
+      this.media_type = data['post'].post_type_id;
+  
+      // Prepend the server base URL to the image/video URL
+      const server_base_url_image = 'http://127.0.0.1:8000/storage/images/';
+      const server_base_url_video = 'http://127.0.0.1:8000/storage/videos/';
+  
+      // Sanitize the image URL, if needed
+      if (this.media_type === 2) {
+        this.post_src_img = this.sanitizer.bypassSecurityTrustUrl(server_base_url_image + data['post'].post_media);
+      } else if (this.media_type === 3) {
+        this.post_src_video = this.sanitizer.bypassSecurityTrustUrl(server_base_url_video + data['post'].post_media);
+      }
     });
-
   }
+  
+  
+  
 
   editPost(){
     this.postService.editPost(this.post_id, this.post_caption).subscribe((data: any) => {
       const parsedResponse = JSON.parse(JSON.stringify(data));
       const status = parsedResponse.status;
       if(status == "success"){
+        this.post_caption='';
+        this.post_src_img='';
+        this.post_src_video='';
         this.alrt.create({
           header: 'Your post has been edited!',
           message: status,
@@ -60,8 +83,8 @@ export class EditPostPage implements OnInit {
 
   }
 
-  async goBack() {
-    const alert = await this.alrt.create({
+  goBack() {
+    this.alrt.create({
       header: 'Confirm',
       message: 'If you leave this page, your changes will be discarded. Are you sure you want to leave?',
       buttons: [
@@ -76,10 +99,7 @@ export class EditPostPage implements OnInit {
           },
         },
       ],
-    });
-  
-    await alert.present();
-  }
-  
-
+    }).then((alert) => alert.present())
+    .catch((err) => console.log(err));
+  };
 }
