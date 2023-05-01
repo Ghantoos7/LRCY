b7d02f06152834ac6f7fc10fb789af9568cebea4
Georgio Ghnatios <94653514+Ghantoos7@users.noreply.github.com>
2023-05-01 13:23:23 +0300

    Testing <> Tested logout API

--- BackEnd/tests/Feature/AdminControllerTest.php
+++ BackEnd/tests/Feature/AdminControllerTest.php
@@ -29,62 +29,76 @@ class AdminControllerTest extends TestCase
     use RefreshDatabase, WithoutMiddleware;
     use WithFaker;
 
+    function testAdminLoginApi()
+    {
+        // Create an admin user
+        $adminUser = Volunteer_user::factory()->create([
+            'user_type_id' => 1,
+            'password' => Hash::make('adminPassword'),
+        ]);
 
-    public function testAdminLogin()
-{
-    // Create an admin user
-    $adminUser = Volunteer_user::factory()->create([
-        'user_type_id' => 1,
-        'password' => Hash::make('adminPassword'),
-    ]);
+        // Create a non-admin user
+        $nonAdminUser = Volunteer_user::factory()->create([
+            'user_type_id' => 2,
+            'password' => Hash::make('nonAdminPassword'),
+        ]);
 
-    // Create a non-admin user
-    $nonAdminUser = Volunteer_user::factory()->create([
-        'user_type_id' => 2,
-        'password' => Hash::make('nonAdminPassword'),
-    ]);
+        // Successful login
+        $response = $this->postJson('/api/v0.1/auth/admin_login', [
+            'organization_id' => $adminUser->organization_id,
+            'password' => 'adminPassword',
+        ]);
+        $response->assertJson(['status' => 'Login successful']);
 
-    // Successful login
-    $response = $this->postJson('/api/v0.1/auth/admin_login', [
-        'organization_id' => $adminUser->organization_id,
-        'password' => 'adminPassword',
-    ]);
-    $response->assertJson(['status' => 'Login successful']);
+        // User not found
+        $response = $this->postJson('/api/v0.1/auth/admin_login', [
+            'organization_id' => 'nonExistentOrganizationId',
+            'password' => 'adminPassword',
+        ]);
+        $response->assertJson(['status' => 'User not found']);
 
-    // User not found
-    $response = $this->postJson('/api/v0.1/auth/admin_login', [
-        'organization_id' => 'nonExistentOrganizationId',
-        'password' => 'adminPassword',
-    ]);
-    $response->assertJson(['status' => 'User not found']);
+        // Permission denied
+        $response = $this->postJson('/api/v0.1/auth/admin_login', [
+            'organization_id' => $nonAdminUser->organization_id,
+            'password' => 'nonAdminPassword',
+        ]);
+        $response->assertJson(['status' => 'Permission denied']);
 
-    // Permission denied
-    $response = $this->postJson('/api/v0.1/auth/admin_login', [
-        'organization_id' => $nonAdminUser->organization_id,
-        'password' => 'nonAdminPassword',
-    ]);
-    $response->assertJson(['status' => 'Permission denied']);
-
-    // Invalid credentials
-    $response = $this->postJson('/api/v0.1/auth/admin_login', [
-        'organization_id' => $adminUser->organization_id,
-        'password' => 'wrongPassword',
-    ]);
-    $response->assertJson(['status' => 'Invalid credentials']);
-
-    // Too many failed login attempts
-    for ($i = 0; $i < 5; $i++) {
-        $this->postJson('/api/v0.1/auth/admin_login', [
+        // Invalid credentials
+        $response = $this->postJson('/api/v0.1/auth/admin_login', [
             'organization_id' => $adminUser->organization_id,
             'password' => 'wrongPassword',
         ]);
-    }
-    $response = $this->postJson('/api/v0.1/auth/admin_login', [
-        'organization_id' => $adminUser->organization_id,
-        'password' => 'adminPassword',
-    ]);
-    $response->assertJson(['status' => 'Too many failed login attempts']);
+        $response->assertJson(['status' => 'Invalid credentials']);
 
-    
-}
+        // Too many failed login attempts
+        for ($i = 0; $i < 5; $i++) {
+            $this->postJson('/api/v0.1/auth/admin_login', [
+                'organization_id' => $adminUser->organization_id,
+                'password' => 'wrongPassword',
+            ]);
+        }
+        $response = $this->postJson('/api/v0.1/auth/admin_login', [
+            'organization_id' => $adminUser->organization_id,
+            'password' => 'adminPassword',
+        ]);
+        $response->assertJson(['status' => 'Too many failed login attempts']);
+    }
+
+    function testLogoutApi()
+    {
+        // Create a user
+        $user = Volunteer_user::factory()->create();
+
+        // Test case: Logout a user
+        $response = $this->actingAs($user)->postJson('/api/v0.1/user/logout');
+
+        $response->assertJsonFragment([
+            'status' => 'Logged out',
+        ]);
+        $response->assertStatus(200);
+
+        // Test case: Logout a non-existent user
+        // Not needed, as the API will require authentication
+    }
 }
